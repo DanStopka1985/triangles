c0f8604a41d550b93a95708909c44f60a52c7dad
DanStopka1985 <danstopka1985@gmail.com>
2024-02-07 01:27:08 +0300

    FIRST

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type triangle struct {
 	generation  int
 	coordinates [6]float32
 	power       float32
+	color       int
 }
 
 func genRandomTriangle() triangle {
@@ -26,35 +27,40 @@ func genRandomTriangle() triangle {
 	return t
 }
 
-func updateContent(c *fyne.Container) {
-	c.RemoveAll()
-	t := genRandomTriangle()
-
+func addTriangleToContainer(c *fyne.Container, t triangle) {
 	pos1 := fyne.Position{t.coordinates[0], t.coordinates[1]}
 	pos2 := fyne.Position{t.coordinates[2], t.coordinates[3]}
 	pos3 := fyne.Position{t.coordinates[4], t.coordinates[5]}
 
 	line1 := canvas.NewLine(color.Black)
 	line1.StrokeColor = color.NRGBA{255, 0, 0, 255}
-	line1.StrokeWidth = 4
+	line1.StrokeWidth = 1
 	line1.Position1 = pos1
 	line1.Position2 = pos2
 
 	line2 := canvas.NewLine(color.Black)
 	line2.StrokeColor = color.NRGBA{255, 0, 0, 255}
-	line2.StrokeWidth = 4
+	line2.StrokeWidth = 1
 	line2.Position1 = pos2
 	line2.Position2 = pos3
 
 	line3 := canvas.NewLine(color.Black)
 	line3.StrokeColor = color.NRGBA{255, 0, 0, 255}
-	line3.StrokeWidth = 4
+	line3.StrokeWidth = 1
 	line3.Position1 = pos3
 	line3.Position2 = pos1
 
 	c.Add(line1)
 	c.Add(line2)
 	c.Add(line3)
+}
+
+func updateContent(c *fyne.Container) {
+	c.RemoveAll()
+	for i := 0; i < 5; i++ {
+		addTriangleToContainer(c, genRandomTriangle())
+	}
+
 	c.Refresh()
 }
 
